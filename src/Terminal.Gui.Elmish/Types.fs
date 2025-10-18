4e8ff5d9f356ee70c33482002db303cc36e2b801
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2025-10-18 17:43:44 +0200

    chore: remove unused code

--- src/Terminal.Gui.Elmish/Types.fs
+++ src/Terminal.Gui.Elmish/Types.fs
@@ -3,29 +3,12 @@
 open System.Collections.Generic
 
 // TODO: could be removed no ?
-type IProperty = interface end
-type IMenuBarProperty = interface inherit IProperty end
-type IMenuProperty = interface inherit IProperty end
 type IStyle = interface end
 
-type ITabProperty = interface inherit IProperty end
-type ITabItemProperty = interface inherit IProperty end
 
 type IMenuBar = interface end
 type IMenuBarItem = interface end
 type IMenu = interface end
-
-type KeyValue =
-    KeyValue of (string * obj)
-        interface IProperty
-        interface IMenuBarProperty
-        interface IMenuProperty
-        interface ITabProperty
-        interface ITabItemProperty
-        interface IMenuBarItem
-        interface IMenuBar
-        interface IMenu
-        interface IStyle
 
 type IProps =
     interface

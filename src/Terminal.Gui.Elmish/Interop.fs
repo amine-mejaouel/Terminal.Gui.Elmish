b778c13f2001be719022e42585bc82c98227579c
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2025-10-18 17:43:44 +0200

    chore: remove unused code

--- src/Terminal.Gui.Elmish/Interop.fs
+++ src/Terminal.Gui.Elmish/Interop.fs
@@ -1,26 +1,14 @@
 ﻿namespace Terminal.Gui.Elmish
 
 open System.Linq.Expressions
-open System.Text
 open System
-open Terminal.Gui
-open Terminal.Gui.Drawing
 open Terminal.Gui.ViewBase
 
 module internal EventHelpers =
 
     open System.Reflection
 
-    type Expr =
-        static member Quote(e:Expression<System.Func<_, _>>) = e
-        static member Quote(e:Expression<System.Action<_>>) = e
-
     let getEventDelegates (eventName:string) (o:obj) =
-        //let eventInfo  = o.GetType().GetEvent(eventName, BindingFlags.Public ||| BindingFlags.NonPublic ||| BindingFlags.Instance ||| BindingFlags.FlattenHierarchy)
-        //if eventInfo |> isNull then
-        //    []
-        //else
-
         let field = o.GetType().GetField(eventName, BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Instance ||| BindingFlags.FlattenHierarchy)
         let baseField = o.GetType().BaseType.GetField(eventName, BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Instance ||| BindingFlags.FlattenHierarchy)
         let eventDelegate =
@@ -60,72 +48,8 @@ module internal EventHelpers =
         else
             delegates |> List.iter (fun d -> eventInfo.AddEventHandler(o, d))
 
-
-
-
 [<RequireQualifiedAccess>]
 module Interop =
-    let inline valueExists name (props: IProperty list) =
-        props
-        |> Seq.cast<KeyValue>
-        |> Seq.exists (function | KeyValue (pname,_) -> name = pname)
-
-    let inline getValueGeneric<'a, 'b when 'b :> IProperty> (name: string) (props: 'b list) =
-        props
-        |> Seq.cast<KeyValue>
-        |> Seq.tryFind (function | KeyValue (pname,_) -> name = pname)
-        |> Option.map (function | KeyValue (_,value) -> value :?> 'a)
-
-    let inline checkValueGeneric<'a, 'b when 'b :> IProperty> (name: string) (props: 'b list) =
-        props
-        |> Seq.cast<KeyValue>
-        |> Seq.tryFind (function | KeyValue (pname,_) -> name = pname)
-        |> Option.map (function | KeyValue (_,value) -> value.GetType() = typeof<'a>)
-
-    let inline getValueDefaultGeneric<'a, 'b when 'b :> IProperty> (name: string) (defaultValue: 'a) (props: 'b list) =
-        props
-        |> getValueGeneric<'a, 'b> name
-        |> Option.defaultValue defaultValue
-
-
-    let inline checkValue<'a> name props = checkValueGeneric<'a, IProperty> name props |> Option.defaultValue false
-
-    let inline mkMenuProp (name:string) (data:obj) : IMenuProperty = KeyValue (name, data)
-    let inline getMenuValue<'a> name (props: IMenuProperty list) = getValueGeneric<'a, IMenuProperty> name props
-    let inline getMenuValueDefault<'a> name defaultVal (props:IMenuProperty list) = getValueDefaultGeneric<'a, IMenuProperty> name defaultVal props
-
-    let inline mkMenuBarProp (name:string) (data:obj) : IMenuBarProperty = KeyValue (name, data)
-    let inline getMenuBarValue<'a> name (props:IMenuBarProperty list) = getValueGeneric<'a, IMenuBarProperty> name props
-    let inline getMenuBarValueDefault<'a> name defaultVal (props:IMenuBarProperty list) = getValueDefaultGeneric<'a, IMenuBarProperty> name defaultVal props
-
-    let mkTabProp (name: string) (data: obj) : ITabProperty = KeyValue (name, data)
-    let getTabValue<'a> name (props: ITabProperty list) = getValueGeneric<'a, ITabProperty> name props
-
-    let mkTabItemProp (name: string) (data: obj) : ITabItemProperty = KeyValue (name, data)
-    let getTabItemValue<'a> name (props: ITabItemProperty list) = getValueGeneric<'a, ITabItemProperty> name props
-
-    let inline mkstyle (name:string) (data:obj) : IStyle= KeyValue (name, data)
-
-    let inline styleExists name (styles:IStyle list) =
-        styles
-        |> Seq.cast<KeyValue>
-        |> Seq.exists (function | KeyValue (pname,_) -> name = pname)
-
-    let inline getStyle<'a> name (styles:IStyle list) =
-        styles
-        |> Seq.cast<KeyValue>
-        |> Seq.tryFind (function | KeyValue (pname,_) -> name = pname)
-        |> Option.map (function | KeyValue (_,value) -> value :?> 'a)
-
-
-    let inline getStyleDefault<'a> name defaultVal (styles:IStyle list) =
-        styles
-        |> Seq.cast<KeyValue>
-        |> Seq.tryFind (function | KeyValue (pname,_) -> name = pname)
-        |> Option.map (function | KeyValue (_,value) -> value :?> 'a)
-        |> Option.defaultValue defaultVal
-
-
     // TODO: move maybe to Props ?
     let filterProps (oldProps: IProps) (newProps: IProps) =
 
@@ -145,14 +69,7 @@ module Interop =
 
         (changedProps, removedProps)
 
-
-    (*let ustr (s:string) = ustring.Make s
-
-    let str (s:ustring) = s.ToString()*)
-
-
     let inline csharpList (list:'a list) = System.Linq.Enumerable.ToList list
-
 
     let rec getParent (view:View) =
         view.SuperView
@@ -164,31 +81,9 @@ module Interop =
                 Some p
         )
 
-    let cloneColorScheme (scheme:Scheme) =
-        let disabled = scheme.Disabled
-        let focus = scheme.Focus
-        let hotFocus = scheme.HotFocus
-        let hotNormal = scheme.HotNormal
-        let normal = scheme.Normal
-        let colorDisabled = Attribute(&disabled)
-        let colorFocus = Attribute(&focus)
-        let colorHotFocus = Attribute(&hotFocus)
-        let colorHotNormal = Attribute(&hotNormal)
-        let colorNormal = Attribute(&normal)
-        Scheme(Disabled=colorDisabled,Focus=colorFocus,HotFocus=colorHotFocus,HotNormal=colorHotNormal,Normal=colorNormal)
-
-
-
-    let removeEventHandlerIfNecessary evName element =
-        let eventDel = EventHelpers.getEventDelegates evName element
-        if (eventDel.Length > 0) then
-            EventHelpers.clearEventDelegates evName element
-
     open Microsoft.FSharp.Quotations
     open Microsoft.FSharp.Quotations.Patterns
-    open Microsoft.FSharp.Quotations.DerivedPatterns
     open FSharp.Linq.RuntimeHelpers
-
 
     /// Get the property name from an event expression
     /// i don'T like it, it's unnecessary complicated to get the name as sting from this particualr expression
@@ -222,7 +117,6 @@ module Interop =
             let event = unbox<IEvent<'a,'b>>(LeafExpressionConverter.EvaluateQuotation eventExpr)
             event.Add(eventHandler)
 
-
     let removeEventHandler (eventExpr: Expr<IEvent<'a,'b>>) element =
         let evName = getPropNameFromEvent eventExpr
         match evName with
@@ -231,10 +125,6 @@ module Interop =
             let eventDel = EventHelpers.getEventDelegates evName element
             if (eventDel.Length > 0) then
                 EventHelpers.clearEventDelegates evName element
-
-
-
-
 
     /// Set an event handler for an event on an element and remove the previous handler
     let setEventHandlerExpr (eventExpr: Expression<Func<_,IEvent<'a,'b>>>) eventHandler element =
@@ -252,12 +142,3 @@ module Interop =
         let event = eventExpr.Compile()
         let e = event.Invoke() // :?> IEvent<'a,'b>
         e.Add(eventHandler)
-
-
-
-
-[<RequireQualifiedAccess>]
-module internal Checker =
-
-    let textChanged (element:View) text =
-        element.Text <> text

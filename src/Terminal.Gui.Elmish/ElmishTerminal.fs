6ac7d4f877f0a53ac0a294111417d9c0905a4682
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2025-12-04 19:00:03 +0100

    feat: wip: expose Application Terminate Msg to clients

--- src/Terminal.Gui.Elmish/ElmishTerminal.fs
+++ src/Terminal.Gui.Elmish/ElmishTerminal.fs
@@ -1,206 +1,216 @@
-[<RequireQualifiedAccess>]
-module Terminal.Gui.Elmish.ElmishTerminal
+namespace Terminal.Gui.Elmish
 
-open System
-open System.Threading.Tasks
-open Elmish
-open Terminal.Gui.App
-open Terminal.Gui.ViewBase
+type TerminalMsg<'a> =
+  | Terminate
+  | Msg of 'a
 
 [<RequireQualifiedAccess>]
-type internal RootView =
-  | Runnable of Runnable
-  | View of View
+module TerminalMsg =
+  let ofMsg msg = TerminalMsg.Msg msg
 
-type internal InternalModel<'model> = {
-  mutable CurrentTreeState: IInternalTerminalElement option
-  mutable Application: IApplication
-  RootView: TaskCompletionSource<RootView>
-  Termination: TaskCompletionSource
-  /// Elmish model provided to the Program by the library caller.
-  ClientModel: 'model
-}
+[<RequireQualifiedAccess>]
+module ElmishTerminal =
 
-module OuterModel =
-  let internal wrapInit (init: 'arg -> 'model * Cmd<'msg>) =
-    fun (arg: 'arg) ->
-      let innerModel, cmd = init arg
+  open System
+  open System.Threading.Tasks
+  open Elmish
+  open Terminal.Gui.App
+  open Terminal.Gui.ViewBase
 
-      let internalModel = {
-        Application = Application.Create()
-        CurrentTreeState = None
-        RootView = TaskCompletionSource<RootView>()
-        Termination = TaskCompletionSource()
-        ClientModel = innerModel
-      }
+  [<RequireQualifiedAccess>]
+  type internal RootView =
+    | Runnable of Runnable
+    | View of View
 
-      internalModel, cmd
+  type internal InternalModel<'model> = {
+    mutable CurrentTreeState: IInternalTerminalElement option
+    mutable Application: IApplication
+    RootView: TaskCompletionSource<RootView>
+    Termination: TaskCompletionSource
+    /// Elmish model provided to the Program by the library caller.
+    ClientModel: 'model
+  }
 
-  let internal wrapUpdate (update: 'msg -> 'model -> 'model * Cmd<'msg>) : 'msg -> InternalModel<'model> -> InternalModel<'model> * Cmd<'msg> =
-    fun (msg: 'msg) (model: InternalModel<'model>) ->
-      let innerModel, cmd =
-        update msg model.ClientModel
+  module OuterModel =
+    let internal wrapInit (init: 'arg -> 'model * Cmd<'msg>) =
+      fun (arg: 'arg) ->
+        let innerModel, cmd = init arg
 
-      { model with ClientModel = innerModel }, cmd
+        let internalModel = {
+          Application = Application.Create()
+          CurrentTreeState = None
+          RootView = TaskCompletionSource<RootView>()
+          Termination = TaskCompletionSource()
+          ClientModel = innerModel
+        }
 
-  let internal wrapView (view: 'model -> Dispatch<'msg> -> ITerminalElement) : InternalModel<'model> -> Dispatch<'msg> -> ITerminalElement =
-    fun (model: InternalModel<'model>) (dispatch: Dispatch<'msg>) -> view model.ClientModel dispatch
+        internalModel, cmd
 
-  let internal wrapSimpleInit (init: 'arg -> 'model) =
-    fun (arg: 'arg) ->
-      let innerModel = init arg
+    let internal wrapUpdate (update: 'msg -> 'model -> 'model * Cmd<'msg>) : 'msg -> InternalModel<'model> -> InternalModel<'model> * Cmd<'msg> =
+      fun (msg: 'msg) (model: InternalModel<'model>) ->
+        let innerModel, cmd =
+          update msg model.ClientModel
 
-      let internalModel = {
-        Application = Application.Create()
-        CurrentTreeState = None
-        RootView = TaskCompletionSource<RootView>()
-        Termination = TaskCompletionSource()
-        ClientModel = innerModel
-      }
+        { model with ClientModel = innerModel }, cmd
 
-      internalModel
+    let internal wrapView (view: 'model -> Dispatch<'msg> -> ITerminalElement) : InternalModel<'model> -> Dispatch<'msg> -> ITerminalElement =
+      fun (model: InternalModel<'model>) (dispatch: Dispatch<'msg>) -> view model.ClientModel dispatch
 
-  let internal wrapSimpleUpdate (update: 'msg -> 'model -> 'model) : 'msg -> InternalModel<'model> -> InternalModel<'model> =
-    fun (msg: 'msg) (model: InternalModel<'model>) ->
-      let innerModel = update msg model.ClientModel
-      { model with ClientModel = innerModel }
+    let internal wrapSimpleInit (init: 'arg -> 'model) =
+      fun (arg: 'arg) ->
+        let innerModel = init arg
 
-  let internal wrapSubscribe (subscribe: 'model -> _) : InternalModel<'model> -> _ =
-    fun outerModel -> subscribe outerModel.ClientModel
+        let internalModel = {
+          Application = Application.Create()
+          CurrentTreeState = None
+          RootView = TaskCompletionSource<RootView>()
+          Termination = TaskCompletionSource()
+          ClientModel = innerModel
+        }
 
-type ElmishTerminalProgram<'arg, 'model, 'msg, 'view> = private ElmishTerminalProgram of Program<'arg, InternalModel<'model>, 'msg, 'view>
+        internalModel
 
-let mutable unitTestMode = false
+    let internal wrapSimpleUpdate (update: 'msg -> 'model -> 'model) : 'msg -> InternalModel<'model> -> InternalModel<'model> =
+      fun (msg: 'msg) (model: InternalModel<'model>) ->
+        let innerModel = update msg model.ClientModel
+        { model with ClientModel = innerModel }
 
-let private setState (view: InternalModel<'model> -> Dispatch<'cmd> -> ITerminalElement) (model: InternalModel<'model>) dispatch =
+    let internal wrapSubscribe (subscribe: 'model -> _) : InternalModel<'model> -> _ =
+      fun outerModel -> subscribe outerModel.ClientModel
 
-  let nextTreeState =
-    match model.CurrentTreeState with
-    | None ->
+  type ElmishTerminalProgram<'arg, 'model, 'msg, 'view> = private ElmishTerminalProgram of Program<'arg, InternalModel<'model>, 'msg, 'view>
 
-      let startState =
-        view model dispatch :?> IInternalTerminalElement
+  let mutable unitTestMode = false
 
-      startState.initializeTree None
+  let private setState (view: InternalModel<'model> -> Dispatch<'cmd> -> ITerminalElement) (model: InternalModel<'model>) dispatch =
 
-      match startState.view with
-      | null -> failwith "error state not initialized"
-      | :? Runnable as r -> model.RootView.SetResult(RootView.Runnable r)
-      | view -> model.RootView.SetResult(RootView.View view)
+    let nextTreeState =
+      match model.CurrentTreeState with
+      | None ->
 
-      startState
+        let startState =
+          view model dispatch :?> IInternalTerminalElement
 
-    | Some currentState ->
-      let nextTreeState =
-        view model dispatch :?> IInternalTerminalElement
+        startState.initializeTree None
 
-      Differ.update currentState nextTreeState
-      nextTreeState
+        match startState.view with
+        | null -> failwith "error state not initialized"
+        | :? Runnable as r -> model.RootView.SetResult(RootView.Runnable r)
+        | view -> model.RootView.SetResult(RootView.View view)
 
-  model.CurrentTreeState <- Some nextTreeState
+        startState
 
-  ()
+      | Some currentState ->
+        let nextTreeState =
+          view model dispatch :?> IInternalTerminalElement
 
-let private terminate model =
-  if not unitTestMode then
-    match model.RootView.Task.Result with
-    | RootView.Runnable r ->
-      r.Dispose()
-      model.Application.RequestStop()
-    | RootView.View _ ->
-      ()
+        Differ.update currentState nextTreeState
+        nextTreeState
 
-  model.Termination.SetResult()
+    model.CurrentTreeState <- Some nextTreeState
 
-let mkProgram (init: 'arg -> 'model * Cmd<'msg>) (update: 'msg -> 'model -> 'model * Cmd<'msg>) (view: 'model -> Dispatch<'msg> -> ITerminalElement) =
-  Program.mkProgram (OuterModel.wrapInit init) (OuterModel.wrapUpdate update) (OuterModel.wrapView view)
-  |> Program.withSetState (setState (OuterModel.wrapView view))
-  |> ElmishTerminalProgram
+    ()
 
-let mkSimple (init: 'arg -> 'model) (update: 'cmd -> 'model -> 'model) (view: 'model -> Dispatch<'cmd> -> ITerminalElement) =
-  Program.mkSimple (OuterModel.wrapSimpleInit init) (OuterModel.wrapSimpleUpdate update) (OuterModel.wrapView view)
-  |> Program.withSetState (setState (OuterModel.wrapView view))
-  |> ElmishTerminalProgram
+  let private terminate model =
+    if not unitTestMode then
+      match model.RootView.Task.Result with
+      | RootView.Runnable r ->
+        r.Dispose()
+        model.Application.RequestStop()
+      | RootView.View _ ->
+        ()
 
-let withSubscription (subscribe: 'model -> Sub<_>) (ElmishTerminalProgram program) =
-  program
-  |> Program.withSubscription (OuterModel.wrapSubscribe subscribe)
-  |> ElmishTerminalProgram
+    model.Termination.SetResult()
 
-let withTermination predicate (ElmishTerminalProgram program) =
-  program
-  |> Program.withTermination predicate terminate
-  |> ElmishTerminalProgram
+  let mkProgram (init: 'arg -> 'model * Cmd<'msg>) (update: 'msg -> 'model -> 'model * Cmd<'msg>) (view: 'model -> Dispatch<'msg> -> ITerminalElement) =
+    Program.mkProgram (OuterModel.wrapInit init) (OuterModel.wrapUpdate update) (OuterModel.wrapView view)
+    |> Program.withSetState (setState (OuterModel.wrapView view))
+    |> ElmishTerminalProgram
 
-let runTerminal (ElmishTerminalProgram program) =
+  let mkSimple (init: 'arg -> 'model) (update: 'cmd -> 'model -> 'model) (view: 'model -> Dispatch<'cmd> -> ITerminalElement) =
+    Program.mkSimple (OuterModel.wrapSimpleInit init) (OuterModel.wrapSimpleUpdate update) (OuterModel.wrapView view)
+    |> Program.withSetState (setState (OuterModel.wrapView view))
+    |> ElmishTerminalProgram
 
-  let waitForStart = TaskCompletionSource()
-  let running = TaskCompletionSource()
-  let mutable waitForTermination = null
+  let withSubscription (subscribe: 'model -> Sub<_>) (ElmishTerminalProgram program) =
+    program
+    |> Program.withSubscription (OuterModel.wrapSubscribe subscribe)
+    |> ElmishTerminalProgram
 
-  let application = Application.Create()
+  let withTermination predicate (ElmishTerminalProgram program) =
+    program
+    |> ElmishTerminalProgram
 
-  let runTerminal (model: InternalModel<_>) =
-    let start dispatch =
-      let rootView = model.RootView.Task.GetAwaiter().GetResult()
-      match rootView with
-      | RootView.Runnable runnable ->
-        if not unitTestMode then
-          Task.Run(fun () ->
-            (
-              try
-                model.Application <- application
-                model.Application.Init() |> ignore
-                model.Application.Run(runnable) |> ignore
-                running.SetResult()
-              with ex -> running.SetException ex
-            )
-            , TaskCreationOptions.LongRunning
-          ) |> ignore
+  let runTerminal (ElmishTerminalProgram program) =
 
-        waitForStart.SetResult()
-        waitForTermination <- model.Termination
-      | _ ->
-        failwith (
-          "`run` is meant to be used for Terminal Elmish loop. " +
-          "For Terminal components with separate Elmish loop, use `runComponent`.")
+    let waitForStart = TaskCompletionSource()
+    let running = TaskCompletionSource()
+    let mutable waitForTermination = null
 
-      { new IDisposable with member _.Dispose() = () }
+    let application = Application.Create()
 
-    start
+    let runTerminal (model: InternalModel<_>) =
+      let start dispatch =
+        let rootView = model.RootView.Task.GetAwaiter().GetResult()
+        match rootView with
+        | RootView.Runnable runnable ->
+          if not unitTestMode then
+            Task.Run(fun () ->
+              (
+                try
+                  model.Application <- application
+                  model.Application.Init() |> ignore
+                  model.Application.Run(runnable) |> ignore
+                  running.SetResult()
+                with ex -> running.SetException ex
+              )
+              , TaskCreationOptions.LongRunning
+            ) |> ignore
 
-  let subscribe model = [ [ "runTerminal" ], runTerminal model ]
+          waitForStart.SetResult()
+          waitForTermination <- model.Termination
+        | _ ->
+          failwith (
+            "`run` is meant to be used for Terminal Elmish loop. " +
+            "For Terminal components with separate Elmish loop, use `runComponent`.")
 
-  program
-  |> Program.withSubscription subscribe
-  |> Program.runWith application
+        { new IDisposable with member _.Dispose() = () }
 
-  waitForStart.Task.GetAwaiter().GetResult()
-  Task.WhenAll(running.Task, waitForTermination.Task).GetAwaiter().GetResult()
+      start
 
-let runComponent (ElmishTerminalProgram program) : ITerminalElement =
+    let subscribe model = [ [ "runTerminal" ], runTerminal model ]
 
-  let waitForView = TaskCompletionSource()
-  let mutable view: ITerminalElement = Unchecked.defaultof<_>
+    program
+    |> Program.withSubscription subscribe
+    |> Program.withTermination (fun msg -> msg = Terminate) terminate
+    |> Program.runWith application
 
-  let runComponent (model: InternalModel<_>) =
-    let start dispatch =
-      (task {
-        // On program startup, Wait for the Elmish loop to take care of creating the root view.
-        let! _ = model.RootView.Task
-        view <- model.CurrentTreeState.Value :> ITerminalElement
-        waitForView.SetResult()
-      }).GetAwaiter().GetResult()
+    waitForStart.Task.GetAwaiter().GetResult()
+    Task.WhenAll(running.Task, waitForTermination.Task).GetAwaiter().GetResult()
 
-      { new IDisposable with member _.Dispose() = () }
+  let runComponent (ElmishTerminalProgram program) : ITerminalElement =
 
-    start
+    let waitForView = TaskCompletionSource()
+    let mutable view: ITerminalElement = Unchecked.defaultof<_>
 
-  let subscribe model = [ [ "runComponent" ], runComponent model ]
+    let runComponent (model: InternalModel<_>) =
+      let start dispatch =
+        (task {
+          // On program startup, Wait for the Elmish loop to take care of creating the root view.
+          let! _ = model.RootView.Task
+          view <- model.CurrentTreeState.Value :> ITerminalElement
+          waitForView.SetResult()
+        }).GetAwaiter().GetResult()
 
-  program
-  |> Program.withSubscription subscribe
-  |> Program.run
+        { new IDisposable with member _.Dispose() = () }
 
-  waitForView.Task.GetAwaiter().GetResult()
-  view
+      start
+
+    let subscribe model = [ [ "runComponent" ], runComponent model ]
+
+    program
+    |> Program.withSubscription subscribe
+    |> Program.run
+
+    waitForView.Task.GetAwaiter().GetResult()
+    view

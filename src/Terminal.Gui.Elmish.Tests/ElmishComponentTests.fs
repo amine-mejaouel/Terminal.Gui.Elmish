6e1a2971399cc03a0a8ef89d032ad6baff9dd36c
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2025-12-07 15:05:32 +0100

    test: TerminalElement returned by an ElmishComponent runComponent should have isElmishComponent = true

---
+++ src/Terminal.Gui.Elmish.Tests/ElmishComponentTests.fs
@@ -0,0 +1,20 @@
+module Terminal.Gui.Elmish.Tests.ElmishComponentTests
+
+open NUnit.Framework
+open Terminal.Gui.Elmish
+open Elmish
+
+[<Test>]
+let ``TerminalElement created by Elmish component is flagged as isElmishComponent`` () =
+
+    let init _ = (), Cmd.none
+    let update _ _ = (), Cmd.none
+
+    let view _ _ = View.button(fun _ -> ())
+
+    let terminalElement =
+      ElmishTerminal.mkSimple init update view
+      |> ElmishTerminal.runComponent
+      :?> IInternalTerminalElement
+
+    Assert.That(terminalElement.isElmishComponent, Is.True)
--- src/Terminal.Gui.Elmish.Tests/Main.fs
+++ src/Terminal.Gui.Elmish.Tests/Main.fs
@@ -1,4 +1,13 @@
 module Main
 
+open NUnit.Framework
+open Terminal.Gui.Elmish
+
+[<SetUpFixture>]
+type AssemblySetup() =
+  [<OneTimeSetUp>]
+  member _.Setup() =
+    ElmishTerminal.unitTestMode <- true
+
 [<EntryPoint>]
 let main _ = 0

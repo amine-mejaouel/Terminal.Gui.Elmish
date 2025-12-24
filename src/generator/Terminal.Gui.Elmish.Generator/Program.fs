a8171279b6710370e523a58e61ff12ab1a147349
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2025-12-25 00:19:54 +0100

    chore: init generator dependencies

--- src/generator/Terminal.Gui.Elmish.Generator/Program.fs
+++ src/generator/Terminal.Gui.Elmish.Generator/Program.fs
@@ -1,2 +1,27 @@
-﻿printfn "Hello from F#"
+﻿open Fabulous.AST
+open type Fabulous.AST.Ast
 
+let viewTypes =
+  typeof<Terminal.Gui.ViewBase.View>.Assembly.GetTypes()
+  |> Array.filter _.IsAssignableTo(typeof<Terminal.Gui.ViewBase.View>)
+
+Oak() {
+  Namespace("Terminal.Gui.Elmish") {
+    Open("System")
+    Open("System.Collections.ObjectModel")
+    Open("Terminal.Gui.ViewBase")
+    Open("Terminal.Gui.Views")
+
+    for viewType in viewTypes do
+      TypeDefn(viewType.Name + "Element", Constructor(ParameterPat("props", "Props"))) {
+          InheritParen("ViewTerminalGuiElement", ConstantExpr("props"))
+          Member("_.name", ConstantExpr($"\"{viewType.Name}\""))
+          Member("_.newView()", ConstantExpr("new " + viewType.FullName + "()"))
+          Member("_.setProps(terminalElement: IInternalTerminalElement, props: Props) =
+      }
+      |> _.toInternal()
+  }
+}
+|> Gen.mkOak // Convert to an Oak AST node
+|> Gen.run // Generate the F# code string
+|> printfn "%s"

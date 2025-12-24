585de662c6fd45920b0286489deb3e471962e536
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2025-12-24 23:47:04 +0100

    chore: remove old generator code

--- src/generator/Terminal.Gui.Elmish.Generator/Program.fs
+++ src/generator/Terminal.Gui.Elmish.Generator/Program.fs
@@ -1,14 +1,2 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿printfn "Hello from F#"
 
-open System
-open System.Diagnostics
-
-//Trace.Listeners.Add(new ConsoleTraceListener());
-#if DEBUG
-Trace.WriteLine("write line")
-#endif
-
-Console.WriteLine("console")
-
-Console.ReadLine() |> ignore

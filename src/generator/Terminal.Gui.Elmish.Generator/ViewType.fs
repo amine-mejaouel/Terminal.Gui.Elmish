20b720ef25da100cbd959c813e2e8a092080bb9d
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2026-01-23 23:31:10 +0100

    refac: code gen: remove dead code

--- src/generator/Terminal.Gui.Elmish.Generator/ViewType.fs
+++ src/generator/Terminal.Gui.Elmish.Generator/ViewType.fs
@@ -5,50 +5,11 @@ open System
 open System.Collections
 open System.Reflection
 
-let private inheritanceChain (viewType: Type) =
-  let rec collectChain (t: Type) (acc: Type list) =
-    match t.BaseType with
-    | null -> acc
-    | baseType when baseType = typeof<Terminal.Gui.ViewBase.View> -> acc
-    | baseType -> collectChain baseType (baseType :: acc)
-
-  collectChain viewType []
-  |> List.map _.Name
-
-let private  isSubChainOf (subChain: 'T list) (superChain: 'T list) =
-  let rec checkChains subChain superChain =
-    match subChain, superChain with
-    | [], _ -> true
-    | _, [] -> false
-    | sh :: st, ph :: pt ->
-        if sh = ph then
-          checkChains st pt
-        else
-          false
-
-  checkChains subChain superChain
-
 let private viewTypes =
   typeof<Terminal.Gui.ViewBase.View>.Assembly.GetTypes()
   |> Seq.filter (fun t -> t.IsAssignableTo(typeof<Terminal.Gui.ViewBase.View>) && t.IsPublic)
   |> Seq.sortBy _.Name
   |> Seq.toList
-
-let private inheritanceChains =
-  viewTypes
-  |> Seq.fold (fun inhChains t ->
-      let tChain = inheritanceChain t
-      let isSubChain = inhChains |> List.exists (isSubChainOf tChain)
-      let isSuperChain = inhChains |> List.exists (fun chain -> isSubChainOf chain tChain)
-      if isSubChain then
-        inhChains
-      else if isSuperChain then
-        inhChains
-        |> List.filter (fun chain -> not (isSubChainOf chain tChain))
-        |> List.append [ tChain ]
-      else
-        inhChains @ [ tChain ]
-  ) []
 
 let viewTypesOrderedByInheritance =
 

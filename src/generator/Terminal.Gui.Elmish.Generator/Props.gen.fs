ffff4b1ef13bbb54f1b7359713686248cee29a0b
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2026-01-25 01:32:44 +0100

    refac: code gen: Props.gen.fs

--- src/generator/Terminal.Gui.Elmish.Generator/Props.gen.fs
+++ src/generator/Terminal.Gui.Elmish.Generator/Props.gen.fs
@@ -20,21 +20,10 @@ let opens = [
     "open Terminal.Gui.Views"
   ]
 
-let gen () =
+let viewSpecificMembers =
   seq {
-    yield "namespace Terminal.Gui.Elmish"
-    yield ""
-    yield! opens
-    yield ""
-    for viewType in Registry.ViewTypes.orderedByInheritance do
-      if viewType.IsGenericType then
-        let genericBlock = genericTypeParamsWithConstraintsBlock viewType
-        yield $"type {getTypeNameWithoutArity viewType}Props{genericBlock}() ="
-      else
-        yield $"type {getTypeNameWithoutArity viewType}Props() ="
-      if viewType = typeof<Terminal.Gui.ViewBase.View> then
-        yield "  member val internal props = Props()"
-        yield """
+    yield "  member val internal props = Props()"
+    yield """
   member this.Children(children: ITerminalElement list) =
     this.props.add (
       PKey.View.children,
@@ -44,37 +33,47 @@ let gen () =
       )
     )
   """
-      else
+    yield "  // Delayed Positions"
+    yield "  member this.X (value: TPos) ="
+    yield "    this.props.add (PKey.View.X_delayedPos, value)"
+    yield ""
+    yield "  member this.Y (value: TPos) ="
+    yield "    this.props.add (PKey.View.Y_delayedPos, value)"
+    yield ""
+  }
+
+let gen () =
+  seq {
+    yield "namespace Terminal.Gui.Elmish"
+    yield ""
+    yield! opens
+    yield ""
+    for viewType in Registry.ViewTypes.orderedByInheritance do
+      let genericBlock = genericTypeParamsWithConstraintsBlock viewType
+      yield $"type {getTypeNameWithoutArity viewType}Props{genericBlock}() ="
+      if viewType <> typeof<Terminal.Gui.ViewBase.View> then
         yield "  inherit ViewProps()"
+      else
+        yield! viewSpecificMembers
 
       let view = ViewMetadata.create viewType
-      let genericBlock = genericTypeParamsBlock viewType
-
-      if view.Type = typeof<Terminal.Gui.ViewBase.View> then
-        yield $"  // Delayed Positions"
-        yield $"  member this.X (value: TPos) ="
-        yield $"    this.props.add (PKey.View.X_delayedPos, value)"
-        yield $""
-        yield $"  member this.Y (value: TPos) ="
-        yield $"    this.props.add (PKey.View.Y_delayedPos, value)"
-        yield $""
 
       if view.Properties.Length > 0 then
         yield "  // Properties"
       for prop in view.Properties do
         yield $"  member this.{prop.PKey} (value: {getFSharpTypeName prop.PropertyInfo.PropertyType}) ="
-        yield $"    this.props.add (PKey.{Registry.ViewTypes.GetUniqueTypeName viewType}{genericBlock}.{prop.PKey}, value)"
+        yield $"    this.props.add ({PKey.getAccessor viewType}.{prop.PKey}, value)"
         yield ""
         if prop.IsViewProperty then
           let valueType = Registry.TEInterfaces.CreateInterface prop.PropertyInfo.PropertyType
           yield $"  member this.{prop.PKey}(value: {valueType}) ="
-          yield $"    this.props.add (PKey.{Registry.ViewTypes.GetUniqueTypeName viewType}{genericBlock}.{prop.PKey}_element, value)"
+          yield $"    this.props.add ({PKey.getAccessor viewType}.{prop.PKey}_element, value)"
         yield ""
       if view.Events.Length > 0 then
         yield "  // Events"
       for event in view.Events do
         yield $"  member this.{event.PKey} (handler: {eventHandlerType event.EventInfo}) ="
-        yield $"    this.props.add (PKey.{Registry.ViewTypes.GetUniqueTypeName viewType}{genericBlock}.{event.PKey}, handler)"
+        yield $"    this.props.add ({PKey.getAccessor viewType}.{event.PKey}, handler)"
         yield ""
   }
   |> CodeWriter.write "Props.gen.fs"

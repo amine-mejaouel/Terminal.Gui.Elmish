43384ca9f342efa96a7a5573f2b3e047a3dd3663
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2026-02-14 02:53:39 +0100

    test: add ID test for Elmish Terminal Component

--- src/Terminal.Gui.Elmish.Tests/TerminalElementIdTests.fs
+++ src/Terminal.Gui.Elmish.Tests/TerminalElementIdTests.fs
@@ -1,6 +1,5 @@
 module Terminal.Gui.Elmish.Tests.TerminalElementIdTests
 
-open Elmish
 open NUnit.Framework
 open Terminal.Gui.Elmish
 
@@ -22,5 +21,32 @@ let ``Simple ID test`` () =
   Assert.Multiple(fun () ->
     Assert.That(view.Id.ToString(), Is.EqualTo("root"))
     Assert.That(label.Id.ToString(), Is.EqualTo("root|child[0]"))
+  )
+
+[<Test>]
+let ``Component ID test`` () =
+  // Arrange
+  let testComp = TestComponent._component (fun p -> p.text "Test Component")
+  let view =
+    View.Runnable [
+      testComp
+    ]
+
+  // Act
+  use _ = ElmishTester.render view
+
+  let testComp = testComp :?> IInternalTerminalElement
+  let view = view :?> IInternalTerminalElement
+
+  // Get the children from the component
+  let children = testComp.Children
+  let label = children.[0]
+  let button = children.[1]
+
+  Assert.Multiple(fun () ->
+    Assert.That(view.Id.ToString(), Is.EqualTo("root"))
+    Assert.That(testComp.Id.ToString(), Is.EqualTo("root|child[0]"))
+    Assert.That(label.Id.ToString(), Is.EqualTo("root|child[0]|child[0]"))
+    Assert.That(button.Id.ToString(), Is.EqualTo("root|child[0]|child[1]"))
   )
 

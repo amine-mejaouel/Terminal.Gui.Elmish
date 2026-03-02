29e348efb1401f036734b4618166e3f3649ae16c
Amine Mejaouel <amine-mejaouel@users.noreply.github.com>
2026-03-02 02:28:47 +0100

    test: add hashcode equality test for PropKey and PropKey<_>

--- src/Terminal.Gui.Elmish.Tests/PropKeyTests.fs
+++ src/Terminal.Gui.Elmish.Tests/PropKeyTests.fs
@@ -11,3 +11,11 @@ let ``PropKey and PropKey<_> are equal when raw key is same`` () =
 
   Assert.That(untyped.Equals typed, Is.True)
   Assert.That(typed.Equals untyped, Is.True)
+
+[<Test>]
+let ``PropKey and PropKey<_> have same hashcode when raw key is same`` () =
+  let rawKey: RawPropKey = "MyView.Parent_element"
+  let untyped: PropKey = { Kind = PropKeyKind.SubElement; Key = rawKey }
+  let typed = PropKey.Create.subElement<IViewTE> rawKey
+
+  Assert.That(untyped.GetHashCode(), Is.EqualTo(typed.GetHashCode()))
